aec2eacafddfe282acbbb5e9b6a32465e292b1d5
Adamouization <aj645@bath.ac.uk>
2018-10-08 18:32:07 +0100

    Improve exit conditions

--- app/video_play.py
+++ app/video_play.py
@@ -1,8 +1,8 @@
 import cv2
 
 
-video_capture = cv2.VideoCapture('../recordings/recording_circle_red_right.mov')
-# video_capture = cv2.VideoCapture('../animations/output/circle_blue_right.avi')
+# video_capture = cv2.VideoCapture('../recordings/recording_circle_red_right.mov')
+video_capture = cv2.VideoCapture('../animations/output/circle_red_left.avi')
 
 if not video_capture.isOpened():
     print("Error opening video file")
@@ -16,8 +16,10 @@ while video_capture.isOpened():
     if ret:
         # display current frame
         cv2.imshow('Frame', frame)
-        # user exit on "q" key press
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+
+        # user exit on "q" or "Esc" key press
+        k = cv2.waitKey(30) & 0xFF
+        if k == 25 or k == 27:
             break
     else:
         break
