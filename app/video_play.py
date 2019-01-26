daf271ca17f8827699c4ed1d18bac16a0f2ae312
Adamouization <adam@jamour.com>
2019-01-26 23:10:14 +0100

    print number of frames in video

--- app/video_play.py
+++ app/video_play.py
@@ -1,13 +1,15 @@
 import cv2
 
-# video_capture = cv2.VideoCapture('../recordings/recording_circle_red_right.mov')
-# video_capture = cv2.VideoCapture('../animations/output/circle_red_left.avi')
 video_capture = cv2.VideoCapture('../footage/1-waves.mp4')
 
 frame_size = (1280, 720)
 
 if not video_capture.isOpened():
     print("Error opening video file")
+
+# print the number of frames in the video
+frame_count = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+print(frame_count)
 
 # read video until completion or user exit
 while video_capture.isOpened():
