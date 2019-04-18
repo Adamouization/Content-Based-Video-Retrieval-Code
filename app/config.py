1cf8da74c70c92d35385ae9dfbc393eca34c11ed
Adamouization <adam@jamour.com>
2019-04-18 14:20:50 +0100

    Top-level comments

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
+"""
+Contains different global variables controlling the system. They set via command line arguments.
+"""
 model = ""  # variable used to specify the histogram model to use
 mode = ""  # variable used to specify the mode to run the program in
 show_histograms = False  # Boolean used to decide whether each generated histogram should be displayed or not
